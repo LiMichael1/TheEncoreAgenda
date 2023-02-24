15042ddadc33ada8955acce787acf14a5f9926cc
Haley Cannon <haleycannon98@gmail.com>
2023-02-24 17:19:45 -0500

    working on calendar events still

--- TheEncoreAgenda/ClientApp/src/components/Calendar/CalendarEvent.jsx
+++ TheEncoreAgenda/ClientApp/src/components/Calendar/CalendarEvent.jsx
@@ -2,7 +2,7 @@
 import { ShowContext, defaultEventInfo } from './ShowContext';
 
 const CalendarEvent = ({ event: {title, description, start, end, allDay } = defaultEventInfo }) => {
-
+    console.log("calendar event component");
     const { setIsOpen, setEventType } = useContext(ShowContext);
     function editEvent() {
         setIsOpen(true);
